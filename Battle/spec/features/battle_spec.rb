07199a4316cfb66c672e748dd82d32d7e9632bed
your name <akram.rasikh@gmail.com>
2016-10-12 16:21:04 +0100

    view hit points

--- Battle/spec/features/battle_spec.rb
+++ Battle/spec/features/battle_spec.rb
@@ -12,3 +12,14 @@ feature 'name input', :type => feature do
     expect(page).to have_text('Anna')
   end
 end
+feature 'score', :type => feature do
+  scenario "user is able to check opponent's score" do
+    visit '/'
+
+    fill_in('player1_name', with: 'Akram')
+    fill_in('player2_name', with: 'Anna')
+    click_button "Submit"
+
+    expect(page).to have_content 'score'
+  end
+end
